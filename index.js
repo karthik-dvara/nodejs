34318bb63f6a1a975ec4ea71228c934721a0ed52
karthik-dvara <114140474+karthik-dvara@users.noreply.github.com>
2023-04-05 18:01:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World- version8';
+  const name = process.env.NAME || 'World- version9';
   res.send(`Hello ${name}!`);
 });
 
